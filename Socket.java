488e5bafb9517374f41c6d8c26f4ffe465a48c29
Bhargav Patel <47592839+iambhargi@users.noreply.github.com>
2019-03-12 23:41:25 +0530

    Update and rename Socket3 to Socket.java

--- Socket.java
+++ Socket.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mca241
- */
 //server creation which will echo the msg sent by the client
 //(single client/server architecture)
 import java.net.ServerSocket;
