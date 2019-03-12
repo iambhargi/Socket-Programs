ac68db532a4787f6a3574727b0ecfbd093ac65ec
Bhargav Patel <47592839+iambhargi@users.noreply.github.com>
2019-03-12 23:40:20 +0530

    Update and rename MultiClientSingleServerDemo to MultiClientSingleServerDemo.java

--- MultiClientSingleServerDemo.java
+++ MultiClientSingleServerDemo.java
@@ -7,16 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mca241
- */
 
 class ThreadServer implements Runnable{
 
